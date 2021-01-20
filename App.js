538df86d9a7a5e1cc62588b876674b7e153e4895
leaderruvuna <ruvunaraymond@gmail.com>
2021-01-20 23:50:17 +0300

    Re-format code

--- App.js
+++ App.js
@@ -1,62 +1,62 @@
 import React from 'react';
 import { createSwitchNavigator, createStackNavigator } from 'react-navigation';
 import { StyleSheet, Text, View } from 'react-native';
-import RiderHome from './components/rider/RiderHome';
-import RiderLogin from './components/rider/RiderLogin';
-import RiderRegister from './components/rider/RiderRegister';
-import RiderVerifyNumber from './components/rider/RiderVerifyNumber';
-import RiderForgotPassword from './components/rider/RiderForgotPassword';
-import DriverHome from './components/driver/DriverHome';
-import DriverLogin from './components/driver/DriverLogin';
-import DriverRegister from './components/driver/DriverRegister';
-import AuthLoadingScreen from './components/main/AuthLoadingScreen';
-import RiderDriverScreenChoice from './components/main/RiderDriverScreenChoice';
-import DriverRegLog from './components/driver/DriverRegLog';
-import RiderRegLog from './components/rider/RiderRegLog';
+import RiderHome from './src/rider/RiderHome';
+import RiderLogin from './src/rider/RiderLogin';
+import RiderRegister from './src/rider/RiderRegister';
+import RiderVerifyNumber from './src/rider/RiderVerifyNumber';
+import RiderForgotPassword from './src/rider/RiderForgotPassword';
+import DriverHome from './src/driver/DriverHome';
+import DriverLogin from './src/driver/DriverLogin';
+import DriverRegister from './src/driver/DriverRegister';
+import AuthLoadingScreen from './src/main/AuthLoadingScreen';
+import RiderDriverScreenChoice from './src/main/RiderDriverScreenChoice';
+import DriverRegLog from './src/driver/DriverRegLog';
+import RiderRegLog from './src/rider/RiderRegLog';
 import * as firebase from 'firebase';
-import ApiKeys from './components/constants/ApiKeys';
+import ApiKeys from './src/constants/ApiKeys';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    firebase.initializeApp(ApiKeys.FirebaseConfig);
-  }
-  render() {
-    return <View style={styles.container} />;
-  }
+	constructor(props) {
+		super(props);
+		firebase.initializeApp(ApiKeys.FirebaseConfig);
+	}
+	render() {
+		return <View style={styles.container} />;
+	}
 }
 
 //
 const AuthStack = createStackNavigator({
-  Home: RiderDriverScreenChoice,
-  //rider
-  RiderScreen: RiderRegLog,
-  RiderReg: RiderRegister,
-  RiderLog: RiderLogin,
-  RiderVerifyNum: RiderVerifyNumber,
-  RiderResetPassWord: RiderForgotPassword,
-  //driver
-  DriverScreen: DriverRegLog,
-  DriverLog: DriverLogin,
-  DriverReg: DriverRegister,
+	Home: RiderDriverScreenChoice,
+	//rider
+	RiderScreen: RiderRegLog,
+	RiderReg: RiderRegister,
+	RiderLog: RiderLogin,
+	RiderVerifyNum: RiderVerifyNumber,
+	RiderResetPassWord: RiderForgotPassword,
+	//driver
+	DriverScreen: DriverRegLog,
+	DriverLog: DriverLogin,
+	DriverReg: DriverRegister
 });
 const AuthStackRider = createStackNavigator({ Rider: RiderHome });
 const AuthStackDriver = createStackNavigator({ Driver: DriverHome });
 export default createSwitchNavigator(
-  {
-    AuthLoading: AuthLoadingScreen,
-    App1: AuthStackRider,
-    App2: AuthStackDriver,
-    Auth: AuthStack,
-  },
-  {
-    initialRouteName: 'AuthLoading',
-  },
+	{
+		AuthLoading: AuthLoadingScreen,
+		App1: AuthStackRider,
+		App2: AuthStackDriver,
+		Auth: AuthStack
+	},
+	{
+		initialRouteName: 'AuthLoading'
+	}
 );
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#1E88E5',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#1E88E5'
+	}
 });
