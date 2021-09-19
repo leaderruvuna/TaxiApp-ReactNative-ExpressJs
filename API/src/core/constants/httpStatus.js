49073a57a0b1d1e6f81f8133653493adf0103cc0
leaderruvuna <ruvunaraymond@gmail.com>
2021-09-19 18:12:09 +0300

    Chore: Initialize taxi API boilerplate

---
+++ API/src/core/constants/httpStatus.js
@@ -0,0 +1,11 @@
+export const HTTP_OK = 200;
+export const HTTP_CREATED = 201;
+
+export const HTTP_BAD_REQUEST = 400;
+export const HTTP_UNAUTHORIZED = 401;
+export const HTTP_ACCESS_DENIED = 403;
+export const HTTP_NOT_FOUND = 404;
+export const HTTP_EXIST = 409;
+
+export const HTTP_SERVER_ERROR = 500;
+export const HTTP_METHOD_NOT_ALLOWED = 500;
