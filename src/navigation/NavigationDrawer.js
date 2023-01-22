536691bd891207642d1753bb51d0e9476fd32bb9
leaderruvuna <ruvunaraymond@gmail.com>
2023-01-22 21:46:58 +0300

    Update drawer and stack navigations to @react-navigation/drawer,/native,/stack

--- src/navigation/NavigationDrawer.js
+++ src/navigation/NavigationDrawer.js
@@ -1,6 +1,9 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native'
 import { createAppContainer } from 'react-navigation';
-import { createDrawerNavigator } from 'react-navigation-drawer';
-import { createStackNavigator } from 'react-navigation-stack';
+// import { createDrawerNavigator } from 'react-navigation-drawer';
+// import { createStackNavigator } from 'react-navigation-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import customDrawerContentComponent from '../components/DrawerComponent/riderDrawerContainer';
 import RiderHistory from '../screens/rider/RiderHistory';
 import RiderSettings from '../screens/rider/RiderSettings';
@@ -14,52 +17,43 @@ import DriverHomeContents from '../screens/driver/DriverHomeContents';
 import DriverSettings from '../screens/driver/DriverSettings';
 import DriverLicence from '../screens/driver/DriverLicence';
 import DriverLogout from '../screens/driver/DriverLogout';
+import { createStackNavigator } from '@react-navigation/stack';
+const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
 
-const RiderHomeStackNav = createStackNavigator(
-   {
-      Main: { screen: RiderHomeContents },
-      pickUpLocation: { screen: RiderPickUp },
-   },
-   {
-      headerMode: 'none',
-      navigationOptions: {
-         headerVisible: false,
-      },
-   },
-);
+const RiderHomeStackNav = () => {
+   return (
+      <Stack.Navigator>
+         <Stack.Screen name="Main" component={RiderHomeContents} />
+         <Stack.Screen name="pickUpLocation" component={RiderPickUp} />
+      </Stack.Navigator>
+   );
+};
 
-const RiderHomeDrawer = createAppContainer(
-   createDrawerNavigator(
-      {
-         Home: { screen: RiderHomeStackNav },
-         Payments: { screen: RiderPayments },
-         Settings: { screen: RiderSettings },
-         Logout: { screen: RiderLogout },
-      },
-      {
-         initialRouteName: 'Home',
-         contentComponent: customDrawerContentComponent,
-         drawerOpenRoute: 'DrawerOpen',
-         drawerCloseRoute: 'DrawerClose',
-         drawerToggleRoute: 'DrawerToggle',
-      },
-   ),
-);
+function RiderHomeDrawer() {
+   return (
+      <NavigationContainer>
+         <Drawer.Navigator>
+            <Drawer.Screen name="Home" component={RiderHomeStackNav} />
+            <Drawer.Screen name="Payments" component={RiderPayments} />
+            <Drawer.Screen name="Settings" component={RiderSettings} />
+            <Drawer.Screen name="Logout" component={RiderLogout} />
+         </Drawer.Navigator>
+      </NavigationContainer>
+   );
+}
 
-const DriverHomeDrawer = createDrawerNavigator(
-   {
-      Home: DriverHomeContents,
-      Settings: DriverSettings,
-      Licence: DriverLicence,
-      Logout: DriverLogout,
-   },
-   {
-      initialRouteName: 'Home',
-      contentComponent: customDrawerContentComponent,
-      drawerOpenRoute: 'DrawerOpen',
-      drawerCloseRoute: 'DrawerClose',
-      drawerToggleRoute: 'DrawerToggle',
-   },
-);
+function DriverHomeDrawer() {
+   return (
+      <NavigationContainer>
+         <Drawer.Navigator>
+            <Drawer.Screen name="Home2" component={DriverHomeContents} />
+            <Drawer.Screen name="Settings2" component={DriverSettings} />
+            <Drawer.Screen name="Licence2" component={DriverLicence} />
+            <Drawer.Screen name="Logout2" component={DriverLogout} />
+         </Drawer.Navigator>
+      </NavigationContainer>
+   );
+}
 
 export { RiderHomeDrawer, DriverHomeDrawer };
