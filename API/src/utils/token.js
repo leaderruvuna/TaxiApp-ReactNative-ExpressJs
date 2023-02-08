796dbbfb45f67221c902d95477d1c44ebb523ba1
leaderruvuna <ruvunaraymond@gmail.com>
2023-02-09 01:27:17 +0300

    Integrate rider token on creation/connection actions

---
+++ API/src/utils/token.js
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken';
+export const createToken = (user,SECRET_KEY) => {
+   return jwt.sign(
+      {
+         firstname: user[0]?.firstname,
+         lastname: user[0]?.lastname,
+         nationality: user[0]?.nationality,
+         image: user[0]?.image,
+         phone_number: user[0]?.phone_number,
+         email: user[0]?.email,
+         date: user[0]?.date,
+      },
+      SECRET_KEY,
+      {
+         expiresIn: '24h',
+      },
+   );
+};
