311580a569d4fc995c83350742eed8080eee708f
leaderruvuna <ruvunaraymond@gmail.com>
2023-02-14 23:55:58 +0300

    Connect Rider mobile profile screen with the API,Capture images using image picker library

--- src/screens/rider/styles/register.js
+++ src/screens/rider/styles/register.js
@@ -12,22 +12,28 @@ export default styles = StyleSheet.create({
       marginTop: 10,
    },
    firstnameContainer: {
-      width: '100%',
+      width: '90%',
       height: 50,
-      paddingHorizontal: 15,
+      marginHorizontal: 5,
       marginTop: 5,
+      borderWidth:0.5,
+      justifyContent:'center'
    },
    lastnameContainer: {
-      width: '100%',
+      width: '90%',
       height: 50,
-      paddingHorizontal: 15,
+      marginHorizontal: 5,
       marginTop: 5,
+      borderWidth:0.5,
+      justifyContent:'center'
    },
    emailContainer: {
-      width: '100%',
+      width: '90%',
       height: 50,
-      paddingHorizontal: 15,
+      marginHorizontal: 5,
       marginTop: 5,
+      borderWidth:0.5,
+      justifyContent:'center'
    },
    registerContainer: {
       width: '100%',
@@ -50,9 +56,24 @@ export default styles = StyleSheet.create({
       width: '100%',
       height: '25%',
       marginTop: 15,
+      flexDirection:'row'
+   },
+   skipButton: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#FFFFFF',
+      borderColor:'#42A5F5',
+      borderWidth:1,
+      width: '100%',
+      height: '25%',
+      marginTop: 15,
    },
    nextText: {
       color: '#ffffff',
+      fontWeight: 'bold',
+   },
+   skipText: {
+      color: '#42A5F5',
       fontWeight: 'bold',
    },
    firstnameInput: {
